261eaa1158b730dc867cfa23ee06363e7c3851a5
evettetelyas <etelyas@gmail.com>
2019-12-18 08:10:38 -0700

    finish symptom endpoint testing

--- test/triage_ex_web/controllers/symptom_controller_test.exs
+++ test/triage_ex_web/controllers/symptom_controller_test.exs
@@ -19,6 +19,35 @@ defmodule Triage.SymptomsControllerTest do
 		]
 	}
 	end
+
+	test "#index with location param renders all symptoms in that location", %{conn: conn} do
+		symptoms = insert_pair(:symptom)
+		conn = get(conn, "/api/v1/symptoms?location=abdomen")
+    
+	  assert json_response(conn, 200) == %{
+		"data" => [
+			%{"common_name" => "Abdominal guarding", 
+				"id" => "#{List.first(symptoms).sid}", 
+				"location" => "abdomen", 
+				"name" => "Abdominal guarding"}, 
+				%{"common_name" => "Abdominal guarding", 
+				"id" => "#{List.last(symptoms).sid}", 
+				"location" => "abdomen", 
+				"name" => "Abdominal guarding"}
+		]
+	}
+	end
+
+	test "#index with location param renders 0 symptoms in that location if there are none", %{conn: conn} do
+		symptoms = insert_pair(:symptom)
+		conn = get(conn, "/api/v1/symptoms?location=head")
+    
+	  assert json_response(conn, 200) == %{
+		"data" => [
+
+		]
+	}
+	end
   
 	test "#show renders a single symptom" do
 	  symptom = insert(:symptom)
