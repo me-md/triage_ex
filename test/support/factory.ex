c165bfd557c5b82316bea79125a746feada61639
evettetelyas <etelyas@gmail.com>
2019-12-18 08:33:07 -0700

    complete request specs

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -11,4 +11,20 @@ defmodule Triage.Factory do
         location: "abdomen"
 	  }
 	end
+
+	def condition_factory do
+		%Triage.Conditions.Condition{
+		  cid: sequence(:cid, &"c_#{&1}"),
+		  name: "Evetteitus Larrypug",
+		  common_name: "Evetteitus",
+		  sex_filter: "both",
+		  category: "Evetteology",
+		  prevalence: "bad",
+		  acuteness: "meh",
+		  severity: "emergency",
+		  icd10_code: "EVT",
+		  hint: "BAD GET 2 DOC",
+		  triage_level: "emergency"
+		}
+	  end
   end
--- test/triage_ex_web/controllers/symptom_controller_test.exs
+++ test/triage_ex_web/controllers/symptom_controller_test.exs
@@ -2,45 +2,45 @@ defmodule Triage.SymptomsControllerTest do
 	use TriageWeb.ConnCase
 	import Triage.Factory
   
-	test "#index renders all symptoms", %{conn: conn} do
+	test "#index renders all symptoms" do
 		symptoms = insert_pair(:symptom)
-		conn = get(conn, "/api/v1/symptoms")
+		conn = get(build_conn(), "/api/v1/symptoms")
     
 	  assert json_response(conn, 200) == %{
 		"data" => [
-			%{"common_name" => "Abdominal guarding", 
-				"id" => "#{List.first(symptoms).sid}", 
-				"location" => "abdomen", 
-				"name" => "Abdominal guarding"}, 
-				%{"common_name" => "Abdominal guarding", 
-				"id" => "#{List.last(symptoms).sid}", 
-				"location" => "abdomen", 
-				"name" => "Abdominal guarding"}
+			%{"common_name" => List.first(symptoms).common_name, 
+				"id" => List.first(symptoms).sid, 
+				"location" => List.first(symptoms).location, 
+				"name" => List.first(symptoms).name}, 
+				%{"common_name" => List.last(symptoms).common_name, 
+				"id" => List.last(symptoms).sid, 
+				"location" => List.last(symptoms).location, 
+				"name" => List.last(symptoms).name}
 		]
 	}
 	end
 
-	test "#index with location param renders all symptoms in that location", %{conn: conn} do
+	test "#index with location param renders all symptoms in that location" do
 		symptoms = insert_pair(:symptom)
-		conn = get(conn, "/api/v1/symptoms?location=abdomen")
+		conn = get(build_conn(), "/api/v1/symptoms?location=abdomen")
     
 	  assert json_response(conn, 200) == %{
 		"data" => [
-			%{"common_name" => "Abdominal guarding", 
-				"id" => "#{List.first(symptoms).sid}", 
-				"location" => "abdomen", 
-				"name" => "Abdominal guarding"}, 
-				%{"common_name" => "Abdominal guarding", 
-				"id" => "#{List.last(symptoms).sid}", 
-				"location" => "abdomen", 
-				"name" => "Abdominal guarding"}
+			%{"common_name" => List.first(symptoms).common_name, 
+				"id" => List.first(symptoms).sid, 
+				"location" => List.first(symptoms).location, 
+				"name" => List.first(symptoms).name}, 
+				%{"common_name" => List.last(symptoms).common_name, 
+				"id" => List.last(symptoms).sid, 
+				"location" => List.last(symptoms).location, 
+				"name" => List.last(symptoms).name}
 		]
 	}
 	end
 
-	test "#index with location param renders 0 symptoms in that location if there are none", %{conn: conn} do
-		symptoms = insert_pair(:symptom)
-		conn = get(conn, "/api/v1/symptoms?location=head")
+	test "#index with location param renders 0 symptoms in that location if there are none" do
+		insert_pair(:symptom)
+		conn = get(build_conn(), "/api/v1/symptoms?location=head")
     
 	  assert json_response(conn, 200) == %{
 		"data" => [
@@ -51,14 +51,14 @@ defmodule Triage.SymptomsControllerTest do
   
 	test "#show renders a single symptom" do
 	  symptom = insert(:symptom)
-	  conn = get(conn, "/api/v1/symptoms/#{symptom.sid}")
+	  conn = get(build_conn(), "/api/v1/symptoms/#{symptom.sid}")
     
 	  assert json_response(conn, 200) == %{
 		"data" =>
-			%{"common_name" => "Abdominal guarding", 
-				"id" => "#{symptom.sid}", 
-				"location" => "abdomen", 
-				"name" => "Abdominal guarding"}
+			%{"common_name" => symptom.common_name, 
+				"id" => symptom.sid, 
+				"location" => symptom.location, 
+				"name" => symptom.name}
 	}
 	end
   end
